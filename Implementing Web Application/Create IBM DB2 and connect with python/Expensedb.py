b7e998dbb4bd9050c03d7e0df604c9f398c311e4
Avinash4122 <69641994+Avinash4122@users.noreply.github.com>
2022-11-19 16:01:50 +0530

    Add files via upload

---
+++ Implementing Web Application/Create IBM DB2 and connect with python/Expensedb.py
@@ -0,0 +1,4 @@
+import ibm_db
+ibm_db.pconnect("DATABASE=bludb;HOSTNAME=ab1a4348f4a4.c3n41cmd0nqnrk39u98g.databases.appdomain.cloud;PORT=31498;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=5;PWD=6;",'','')
+print("connection successful...")
+
